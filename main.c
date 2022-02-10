d03984bf4f3141ce1471b829e4d7cdb89dcf6344
ahmadaziz6720 <a715210@gmail.com>
2022-02-10 13:19:20 +0700

    input

--- main.c
+++ main.c
@@ -22,21 +22,31 @@ int main()
     int jml=0;
     printf("Masukkan Jumlah Mahasiswa: ");
     scanf("%d", &jml);
+    printf("\n");
 
-    struct mahasiswa ke[3] = {{"Ahmad Aziz", 13220034, 99.9}};
+    struct mahasiswa ke[40] = {{"Ahmad Aziz", 13220034, 99.9}};
 
-    for (int i=0; i<jml-1; ++i){
+    for (int i=0; i<=jml-1; ++i){
+        printf("Masukkan data mahasiswa ke-%d", i+1);
+        printf("\n");
         printf("Nama: ");
         scanf("%s", &ke[i].nama);
         printf("NIM: ");
         scanf("%d", &ke[i].nim);
         printf("Kehadiran: ");
-        scanf("%d", &ke[i].kehadiran);
+        scanf("%f", &ke[i].kehadiran);
+        printf("\n");
     }
+    printf("Berikut adalah data mahasiswa dengan kehadiran kurang dari 80%\n\n");
 
-    printf("nama: %s\n", ke[1].nama);
-    printf("NIM: %d\n", ke[1].nim);
-    printf("kehadiran: %.2f\n", ke[1].kehadiran);
-
+    for (int i=0; i<=jml-1; ++i){
+        if(ke[i].kehadiran<80.0){
+            printf("nama: %s\n", ke[i].nama);
+            printf("NIM: %d\n", ke[i].nim);
+            printf("kehadiran: %.2f", ke[i].kehadiran);
+            printf("%");
+            printf("\n");
+        }
+    }
     return 0;
 }
